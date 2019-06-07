3b611bcb30a634c14fe3ee50c82a0a06fa312db9
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-07 09:39:23 +0530

    Create 5a.py

---
+++ 5a.py
@@ -0,0 +1,4 @@
+inp1=input()
+List1=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+if inp1 in Lis1t:
+  print(List1.index(inp1))
