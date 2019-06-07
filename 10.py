b064190baf662762bf5b0cc1062e7c3910fa585b
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-07 09:54:35 +0530

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,12 @@
+x1,y1=input().split()
+z1=0
+if leng(x1)!=leng(y1):
+    print("no")
+else:
+    for x in range(leng(x1)):
+        if x1[x]!=y1[x]
+            z1+=1
+if z1==1:
+    print("yes")
+else:
+    print("no")
